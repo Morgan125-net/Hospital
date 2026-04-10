ab8b13d6d17d49d364151fb7f610396d57390d5d
Morgan125-net <murmorgan125@gmail.com>
2026-04-10 19:16:57 +0300

    Commit changes

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 require("./jobs/reminder.job");
 
 const app = require("./app");
-
-const PORT = process.env.PORT || 5000;
-
 const connectDB = require("./config/db");
 
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
